890db066e533793f9a21b42d0f258e52b8ce7687
zedl3all <sanguanwongtanapat@gmail.com>
2025-02-26 23:06:26 +0700

    style: update CSS for layout adjustments and improve responsiveness; enhance navbar padding

--- Views/Home/home.js
+++ Views/Home/home.js
@@ -6,6 +6,32 @@ $(document).ready(function () {
         autoplaySpeed: 2000,
         prevArrow: '<button type="button" class="slick-prev"><span class="glyphicon glyphicon-chevron-left"></span></button>',
         nextArrow: '<button type="button" class="slick-next"><span class="glyphicon glyphicon-chevron-right"></span></button>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
     });
 });
 
@@ -17,5 +43,7 @@ $(document).ready(function () {
         autoplaySpeed: 2000,
         prevArrow: false,
         nextArrow: false,
+        draggable: false,
+        swipe: false,
     });
 });
