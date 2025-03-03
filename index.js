2f24927b69bc5eb794c0cf4ee485ee9788ee37eb
Teetat <66070092@kmitl.ac.th>
2025-03-03 17:47:12 +0700

    Update ProductAll

--- index.js
+++ index.js
@@ -152,7 +152,26 @@ app.get("/", (req, res) => {
 });
 
 app.get("/ProductAll", (req, res) => {
-    res.redirect('/ProductAll/1');
+    const categoriesQuery = 'SELECT * FROM categories';
+    const productsQuery = `SELECT * FROM products;`;
+
+    db.all(categoriesQuery, [], (err, categories) => {
+        if (err) {
+            console.error(`Error fetching categories: ${err.message}`);
+            res.status(500).send('Failed to fetch categories');
+            return;
+        }
+
+        db.all(productsQuery, [], (err, products) => {
+            if (err) {
+                console.error(`Error fetching products: ${err.message}`);
+                res.status(500).send('Failed to fetch products');
+                return;
+            }
+                const cat = [{name:"All Product", id:0}];
+                res.render('ProductAll/ProductAll', { categories, products, cat});
+        });
+    });
 });
 
 app.get("/ProductAll/:id", (req, res) => {
